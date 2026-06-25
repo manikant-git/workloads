26ca07ce86880df2c88719305b0c1be17bbbac1d
Kavulapalli Manikanta <manikant14568@gmail.com>
2026-06-25 16:59:59 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_1_cidr" {
+  type = string
+}
+
+variable "public_subnet_2_cidr" {
+  type = string
+}
