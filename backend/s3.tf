28810a7636a9bd5bfee8867790e808209d25ec47
Kavulapalli Manikanta <manikant14568@gmail.com>
2026-06-25 16:44:04 +0530

    Create s3.tf

---
+++ backend/s3.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "manikanta-terraform-state-12345"
+
+  tags = {
+    Name = "terraform-state"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
